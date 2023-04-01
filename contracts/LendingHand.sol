e323d4dadf562cf82eaeda74f11b3e11cc469e80
dogeyboy1932 <vg23@illinois.edu>
2023-04-01 14:39:38 -0500

    Resolving merge conflicts

--- contracts/LendingHand.sol
+++ contracts/LendingHand.sol
@@ -1,9 +1,7 @@
 pragma solidity ^0.8.17;
-
+import "hardhat/console.sol";
 
 contract LendingHand {
-
-
 // profile info
     struct Profile {
         address user;
@@ -38,12 +36,23 @@ contract LendingHand {
 
     //receivers can make a post
     //@Yug
-    function createPost() public {
+    function createPost(address receiver, string calldata title, string calldata description, uint256 goal, uint256 amount_raised, string calldata deadline, uint256 num_donors) public {
         // initialize post object, 
         //define attributes of post, so fill in user provided receiver address, title, description, etc.
         // create post_id, should just be index of the receiver's posts array idx.
         //push it to array of receiver's posts
-        
+        //Note: Currently assumes address is in post
+
+        Post memory post;
+        post.receiver = receiver;
+        post.title = title;
+        post.description = description;
+        post.goal = goal;
+        post.amount_raised = amount_raised;
+        post.deadline = deadline;
+        post.num_donors = num_donors;
+        post.post_id = posts_list[receiver].length;
+        posts_list[receiver].push(post);
     }
 
     // update posts attributes,  need to add more parameters
@@ -53,8 +62,6 @@ contract LendingHand {
         // change all attributes to ones passed by user
     }
 
-    
-    
     // deletes post, removes it from receivers array
     // add paramters
     //@akhil
@@ -85,14 +92,9 @@ contract LendingHand {
 // NEW @Akhil
 
 
-    
-    
-    
     //if you want to give money
     //  @manas
     function fundPost(address receiver, int post_id) public{
         // fund receiver
         //update post with new amount
-    }
-    
 }
