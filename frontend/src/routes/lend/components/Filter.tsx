1c2b663495d59872b81724206211965243fe4c8f
Manas <manaspgandhi@gmail.com>
2023-04-13 16:40:32 -0500

    basic filter done

--- frontend/src/routes/lend/components/Filter.tsx
+++ frontend/src/routes/lend/components/Filter.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from 'react';
+import { useState } from "react";
+import "./Filter.css"
 
 interface FilterProps {
 
 }
 
 export const Filter: React.FC<FilterProps> = ({}) => {
-        return (<></>);
+        const [isOpen, setIsOpen] = useState(false);
+        
+        return (
+        <>
+                <div className="outer-box">
+
+                        <div className="item-wrapper">
+                                <div className="check-box-wrapper">
+                                        <div className="check-box"></div>
+                                </div>
+                                <div className="check-box-text-wrapper">item 1</div>
+                        </div>
+                        <div className="item-wrapper">
+                                <div className="check-box-wrapper">
+                                        <div className="check-box"></div>
+                                </div>
+                                <div className="check-box-text-wrapper">item 2</div>
+                        </div>
+                        <div className="item-wrapper">
+                                <div className="check-box-wrapper">
+                                        <div className="check-box"></div>
+                                </div>
+                                <div className="check-box-text-wrapper">item 3</div>
+                        </div>
+                        <div className="item-wrapper">
+                                <div className="check-box-wrapper">
+                                        <div className="check-box"></div>
+                                </div>
+                                <div className="check-box-text-wrapper">item 4</div>
+                        </div>
+                        <div className="item-wrapper">
+                                <div className="check-box-wrapper">
+                                        <div className="check-box"></div>
+                                </div>
+                                <div className="check-box-text-wrapper">item 5</div>
+                        </div>
+                        <div className="item-wrapper">
+                                <div className="check-box-wrapper">
+                                        <div className="check-box"></div>
+                                </div>
+                                <div className="check-box-text-wrapper">item 6</div>
+                        </div>
+                        <div className="item-wrapper">
+                                <div className="check-box-wrapper">
+                                        <div className="check-box"></div>
+                                </div>
+                                <div className="check-box-text-wrapper">item 7</div>
+                        </div>
+                        <div className="item-wrapper">
+                                <div className="check-box-wrapper">
+                                        <div className="check-box"></div>
+                                </div>
+                                <div className="check-box-text-wrapper">item 8</div>
+                        </div>
+                        
+                </div>
+        </>
+        );
 }
--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import "../index.css"
+import {Filter} from "../routes/lend/components/Filter"
 
 interface rootProps {}
 
 export const Root: React.FC<rootProps> = ({}) => {
   return (
     <>
-      this is the root, it serves as a container for all your other pages as
-      well as a place for global components. the navbar should go here because
-      you want it to be rendered on every page.
+    <Filter />
+      {/* <h1>
+        this is the root, it serves as a container for all your other pages as
+        well as a place for global components. the navbar should go here because
+        you want it to be rendered on every page.
+      </h1> */}
     </>
   );
 };
