27cfdd99a83ad7d4170c599ab9d493d6235f7706
Saumya Madhan <smadhan2@illinois.edu>
2023-04-19 14:38:33 -0500

    styles for filter

--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -11,9 +11,7 @@ export const Root: React.FC<rootProps> = ({}) => {
     <>
     <Filter />
     <NavBar />
-    <h1>this is the root, it serves as a container for all your other pages as
-      well as a place for global components. the navbar should go here because
-      you want it to be rendered on every page</h1>
+
     </>
   );
 };
