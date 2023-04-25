6139f633990557577a356f48940d548d589d2459
Saumya Madhan <smadhan2@illinois.edu>
2023-04-24 19:16:03 -0500

    added getListings

---
+++ frontend/util/getListings.ts
@@ -0,0 +1,5 @@
+function getListings() {
+    // return an array of post objects representing all listings 
+    // call the contract
+    // format the posts list into a card array and return it
+}
