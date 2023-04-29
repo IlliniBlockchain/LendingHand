e7edac735ad914365624a8b8347a1db9e2cf87c7
dogeyboy1932 <vg23@illinois.edu>
2023-04-29 01:11:36 -0500

    Connect Wallet Button

--- frontend/src/components/ConnectWallet.tsx
+++ frontend/src/components/ConnectWallet.tsx
@@ -2,6 +2,7 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 
 import { useState } from 'react';
+import { ethers } from 'ethers';
 
 export default function ConnectButton() {
     const [walletAddress, setWalletAddress] = useState("This");
@@ -28,20 +29,32 @@ export default function ConnectButton() {
         */
     
       );
+
+      async function ConnectedContract() {
+        
+      }
     
     
+    //   0x74eD261E8ec5cf49170748F8D1B0b91c39665b53
+
       async function requestAccount() {
     
         if(window.ethereum) {
           try {
-            const accounts = await window.ethereum.request({
-              method: "eth_requestAccounts",
-            });
-            console.log(accounts);
-    
-            setWalletAddress(accounts[0]);
-
-          } catch (error) {
+        
+        
+        
+            const provider = new ethers.BrowserProvider(window.ethereum);
+            
+            // Prompt user for account connections
+            await provider.send("eth_requestAccounts", []);
+            const signer = provider.getSigner();
+            console.log("Account:", (await signer).getAddress());
+            
+            // console.log(provider.getSigner());
+        
+        
+        } catch (error) {
             console.log('Error Connecting...')
           }
           
@@ -51,4 +64,3 @@ export default function ConnectButton() {
       } 
 
   }
-
